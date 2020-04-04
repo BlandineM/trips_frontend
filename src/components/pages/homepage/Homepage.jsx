402c034bf8af12aa2cec5758b96a642fde378998
Blandine <blandine.mallart@gmail.com>
2020-04-04 17:28:17 +0200

    feat(homepage) change the contenue

--- src/components/pages/homepage/Homepage.jsx
+++ src/components/pages/homepage/Homepage.jsx
@@ -5,6 +5,14 @@ import { NavLink } from "react-router-dom";
 function Homepage() {
   return (
     <div className="homepage">
+      <div>
+
+        <img className="banniere" src="https://res.cloudinary.com/blandine/image/upload/v1586012438/homepage.jpg"></img>
+      </div>
+      <div className="info">
+        <h1 className="title_homepage">Bien préparer son voyage</h1>
+        <p>L'organisation d'un voyage suscite de nombreuses questions : <br /><br />Où aller ?<br /> Quand partir ? <br />Quel budget ? <br />Combien de temps ? <br /><br />Trip's Harley va vous aider à trouver le bon endroit, au bon moment. Avec l'aide des Tripeurs vous pouvez voir qui à visiter quoi au même moment. Ainsi partager vos meilleurs découvertes. Ce qu'il vous restera seulement à faire, c'est de profiter!</p>
+      </div>
       <div className="card">
         <div className="sun">
           <NavLink to="/plage">
