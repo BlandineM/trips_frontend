20358f6692492f785b94afc7a395af7009c09f4c
Blandine <blandine.mallart@gmail.com>
2020-04-05 20:29:12 +0200

    feat(header) change the size of header and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,16 @@ export default function Header() {
       <style>
         @import url('https://fonts.googleapis.com/css?family=Lora:400,500,600,700&display=swap');
 </style>
-      <a href="/">
-        <img src="https://res.cloudinary.com/blandine/image/upload/v1585598420/Harley_s_trip.png"></img>
-      </a>
-      <ul>
-        <li>Accueil</li>
-        <li>Mon profil</li>
-      </ul>
+      <div className="logo">
+        <a href="/">
+          <img src="https://res.cloudinary.com/blandine/image/upload/v1586076479/harley_trip_rogner.png"></img>
+        </a>
+      </div>
+      <div className="nav">
+        <ul>
+          <li>Mon profil</li>
+        </ul>
+      </div>
     </div>
   );
 }
