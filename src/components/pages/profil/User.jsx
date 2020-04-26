6a5be021a685648ca7ee29cf34032e67ade1cfa0
Blandine <blandine.mallart@gmail.com>
2020-04-26 11:48:56 +0200

    feat(updateAvatar) try in hooks

--- src/components/pages/profil/User.jsx
+++ src/components/pages/profil/User.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Axios from 'axios';
+import axios from 'axios';
 import "./user.scss"
 // import Data from '/data/data.json'
 import {
@@ -12,19 +12,24 @@ import {
   Annotation,
   ZoomableGroup
 } from "react-simple-maps";
+import UpdateAvatar from './UpdateAvatar';
 const { apiSite } = require("../../../conf")
 
 function User() {
   const [profil, setProfil] = useState([]);
   const [choice, setChoice] = useState('map');
   const [check, setCheck] = useState('fait');
+  const [truc, setTruc] = useState('');
   const toPassed = useSelector(state => state.LastTrip);
   const toNext = useSelector(state => state.NextTrip);
-  const [test, setTest] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null);
   const user = useSelector(state => state.user);
-  const dispatch = useDispatch();
   const token = useSelector(state => state.user.token);
+  const dispatch = useDispatch();
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
 
   const geoUrl =
     "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
@@ -46,45 +51,92 @@ function User() {
 
 
   useEffect(() => {
-    Axios.get(`${apiSite}/profil/${user.id}`, {
-      headers: { Authorization: `Bearer ${token}` }
+    axios.get(`${apiSite}/profil/2`, {
+      // headers: { Authorization: `Bearer ${token}` }
     }).then(({ data }) => {
       setProfil(data);
       dispatch({ type: "DATA_LAST_TRIP", data: data.countries });
     });
 
-    Axios.get(`${apiSite}/nextTrip/user/2`, {
+    axios.get(`${apiSite}/nextTrip/user/2`, {
       // headers: { Authorization: `Bearer ${token}` }
     }).then(({ data }) => {
       dispatch({ type: "DATA_NEXT_TRIP", data: data });
     });
 
-  }, [dispatch, token, user.id]);
+  }, [dispatch]);
 
-  function fileSelectedHandler(e) {
+  const onChange = e => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
+
+  const onSubmit = async e => {
     e.preventDefault();
-    setSelectedFile(e.target.files[0])
-    fileUploadHandler(e);
-    return setSelectedFile(false);
-  }
-
-  function fileUploadHandler(e) {
-    e.persist();
-    const { selectedFile } = this.state;
-    const { refreshProfile } = this.props;
     const formData = new FormData();
-    const config = {
-      headers: { 'content-type': 'multipart/form-data' },
-      timeout: 6000
-    };
-    formData.append('avatar', selectedFile);
-    Axios.post(`${apiSite}/users/${user.id}/avatar`, formData, config)
-      .then(refreshProfile)
-      .catch(console.log);
-  }
+    formData.append('file', file);
 
   const codeVisited = toPassed.map((c) => { return c.code })
   const codeToVisit = toNext.map((c) => { return c.code })
+    try {
+      const res = await axios.post(`${apiSite}/profil/2/avatar`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        onUploadProgress: progressEvent => {
+          // setUploadPercentage(
+          //   parseInt(
+          //     Math.round((progressEvent.loaded * 100) / progressEvent.total)
+          //   )
+          // );
+
+          // Clear percentage
+          // setTimeout(() => setUploadPercentage(0), 10000);
+        }
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+
+      setMessage('File Uploaded');
+    } catch (err) {
+      if (err.response.status === 500) {
+        setMessage('There was a problem with the server');
+      } else {
+        setMessage(err.response.data.msg);
+      }
+    }
+  };
+
+
+  // function fileUploadHandler(e) {
+  //   console.log('fileup 1');
+  //   console.log("select file", selectedFile)
+  //   e.persist();
+  //   // const { selectedFile } = this.state;
+  //   // const { refreshProfile } = this.props;
+  //   const formData = new FormData();
+  //   const config = {
+  //     headers: { 'content-type': 'multipart/form-data' },
+  //     timeout: 6000
+  //   };
+  //   formData.append('avatar', selectedFile);
+  //   return axios.post(`${apiSite}/profil/2/avatar`, formData, config)
+  //     .then(user => {
+  //       console.log('fileup 2');
+  //       return user
+  //     })
+
+  //     .catch(console.log);
+
+  // }
+
+  const codeVisited = toPassed.map((c) => { return c.code })
+  const codeToVisit = toNext.map((c) => { return c.code })
+  // console.log(selectedFile, "---------------------file select");
+
+
   return (
 
     <div className="container_profil">
@@ -93,9 +145,45 @@ function User() {
         <h1 className="title_user">Informations:</h1>
 
         <div className="profil">
-          <h2 className="name">{profil.name}</h2>
+          <form onSubmit={onSubmit}>
+            <div className='custom-file mb-4'>
+              <input
+                type='file'
+                className='custom-file-input'
+                id='customFile'
+                onChange={onChange}
+              />
+              <label className='custom-file-label' htmlFor='customFile'>
+                {filename}
+              </label>
+            </div>
+
+            {/* <Progress percentage={uploadPercentage} /> */}
+
+            <input
+              type='submit'
+              value='Upload'
+              className='btn btn-primary btn-block mt-4'
+            />
+          </form>
+          {uploadedFile ? (
+            <div className='row mt-5'>
+              <div className='col-md-6 m-auto'>
+                <h3 className='text-center'>{uploadedFile.fileName}</h3>
+                <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+              </div>
+            </div>
+          ) : null}
+          <div>
+            <h2 className="name">{profil.name}</h2>
+            <h2 className="name">age</h2>
+            <p className="name">Description</p>
+            <img src="./pen.png" alt="" />
+          </div>
           <div className="picture">
-            <img src={(profil.avatar != null
+            {/* <UpdateAvatar avatar={profil.avatar} >
+            </UpdateAvatar> */}
+            {/* <img src={(profil.avatar != null
               ? `${profil.avatar}`
               : 'https://res.cloudinary.com/blandine/image/upload/v1585844046/avatar/none.png')}
               alt='image de profil'></img>
@@ -105,9 +193,9 @@ function User() {
               name="avatar"
               accept="image/x-png,image/gif,image/jpeg"
               onChange={(e) => { fileSelectedHandler(e); }}
-            // ref={(fileInput) => { this.fileInput = fileInput; }}
+              ref={(fileInput) => { setTruc(fileInput) }}
             />
-            <button type="button" >Modifier mon avatar</button>
+            <button type="button" onClick={() => truc.click()} className="btn btn-secondary">Modifier mon avatar</button> */}
           </div>
         </div>
 
