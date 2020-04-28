dd1995f22376b1d29b68e04d00b2fa6b8c8d1f7a
Blandine <blandine.mallart@gmail.com>
2020-04-28 21:47:45 +0200

    feat(userPage) add lastTrip in component

--- src/components/pages/profil/nextTrip/NextTrip.jsx
+++ src/components/pages/profil/nextTrip/NextTrip.jsx
@@ -4,6 +4,7 @@ import "./nextTrip.scss"
 
 function NextTrip() {
   const toNext = useSelector(state => state.NextTrip);
+  const toPassed = useSelector(state => state.LastTrip);
   const mois = [
     "Janvier",
     "Février",
@@ -20,21 +21,47 @@ function NextTrip() {
   ]
 
   return (
-    toNext.map((test, i) => {
-      if (i === 0) {
-        return (
-          <div className="nextTrip">
-            <div className="date">
-              <h1>{mois[test.periode_month]}</h1>
-              {test.year != null ? (<h1>{test.year}</h1>) : (<h1>Année à définir</h1>)}
+    <div>
+      {toNext.map((country, i) => {
+        if (i === 0) {
+          return (
+            <div className="nextTrip">
+              <div className="date">
+                <h1>{mois[country.periode_month]}</h1>
+                {country.year != null ? (<h1>{country.year}</h1>) : (<h1>Année à définir</h1>)}
+              </div>
+              <div className="pays">
+                <img src={country.pays_flag} alt="flag of country" />
+                <div>
+                  <h1 className="country_name">{country.pays_name}</h1>
+                  <h2 className="compte">Dans 1 mois</h2>
+                </div>
+              </div>
             </div>
-            <div className="pays">
-              <h1>{test.pays_name}</h1>
+          )
+        }
+      })}
+
+      {toPassed.map((countryLast, i) => {
+        if (i === 0) {
+          return (
+            <div className="lastTrip">
+              <div className="pays">
+                <div>
+                  <h1 className="country_name">{countryLast.pays_name}</h1>
+                  <h2 className="compte">Il y a  1 mois</h2>
+                </div>
+                <img src={countryLast.pays_flag} alt="flag of country" />
+              </div>
+              <div className="date">
+                <h1>{mois[countryLast.periode_month]}</h1>
+                {countryLast.year != null ? (<h1>{countryLast.year}</h1>) : (<h1>Année à définir</h1>)}
+              </div>
             </div>
-          </div>
-        )
-      }
-    })
+          )
+        }
+      })}
+    </div>
   )
 
 }
