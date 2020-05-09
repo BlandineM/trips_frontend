4d7e18486ea25f1128d9b7ea0086c43c0d91c427
Blandine <blandine.mallart@gmail.com>
2020-05-09 12:59:15 +0200

    feat(header) toogle when connect or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./header.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Axios from 'axios';
+const { apiSite } = require("../conf");
 
 export default function Header() {
+  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const isLogin = () => {
+    if (localStorage.getItem('token') != null) {
+      return true
+    }
+    return false
+  }
+  useEffect(() => {
+
+    Axios.get(`${apiSite}/${user.id}`)
+      .then(({ data }) => {
+        dispatch({ type: "FETCHING_USER_DATA", value: data })
+      })
+  }, [dispatch, user.id])
+  console.log(user);
+
   return (
     <div className="header">
+      {isLogin()}
       <style>
         @import url('https://fonts.googleapis.com/css?family=Lora:400,500,600,700&display=swap');
 </style>
@@ -14,7 +36,14 @@ export default function Header() {
       </div>
       <div className="nav">
         <ul>
-          <li>Mon profil</li>
+          {isLogin()
+            ? <Link to="/profil">
+              <li>{user.avatar}</li>
+            </Link>
+            : <Link to="/connexion">
+              <li>Se connecter</li>
+            </Link>
+          }
         </ul>
       </div>
     </div>
