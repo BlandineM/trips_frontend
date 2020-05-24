91d388a3a87eea92cb7496f6a3e396b386a46ab5
Blandine <blandine.mallart@gmail.com>
2020-05-24 16:21:40 +0200

    feat(newTrip) add from for add a trip check or no

--- src/components/pages/profil/NewTrip.jsx
+++ src/components/pages/profil/NewTrip.jsx
@@ -1,30 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import Axios from 'axios';
 import "./newTrip.scss"
 
+const { apiSite } = require("../../../conf")
 function NewTrip() {
   const [pays, setPays] = useState([]);
   const [year, setYear] = useState([]);
-  const [month, setMonth] = useState("");
-  const [idUser, setIdUser] = useState("2");
+  const [month, setMonth] = useState();
+  const [check, setCheck] = useState(null);
   const [countries, setCountries] = useState([]);
-  const [visitedCountry, setVisitedCountry] = useState([]);
+  const toPassed = useSelector(state => state.LastTrip);
+  const user = useSelector(state => state.user);
+
+  const mois = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"
+  ]
 
   useEffect(() => {
-    Axios.get(`http://localhost:5000/countries`).then(({ data }) => {
+    Axios.get(`${apiSite}/countries`).then(({ data }) => {
       setCountries(data)
-    });
-    Axios.get(`http://localhost:5000/profil/2`).then(({ data }) => {
-      setVisitedCountry(data.countries.map((country) => { return (country.pays_name) }));
     });
   }, [])
 
   function valider(e) {
-    Axios.post(`http://localhost:5000/2/country`, {
+    Axios.post(`${apiSite}/profil/${user.id}/trip`, {
       id_pays: pays,
       id_periodes: month,
       year,
-      id_users: 2
+      check
     })
   }
 
@@ -34,7 +49,7 @@ function NewTrip() {
       <div className="lastTrip">
         <h1>Tes derniers voyages:</h1>
         <ul>
-          {visitedCountry.map((visit) => {
+          {toPassed.map((visit) => {
             return (
               <li>{visit}</li>
             )
@@ -51,35 +66,38 @@ function NewTrip() {
           <div className="country">
             <h1>Pays:</h1>
             <label htmlFor="countries"></label>
-            <select name="countries" id="countries">
+            <input list="countries" onChange={evt => setPays(evt.target.value)} />
+            <datalist name="countries" id="countries">
               {countries.map((country) => {
-                return (<option value={country.id_pays} onChange={evt => setPays(evt.target.value)}>{country.nameFr}</option>)
+                return (<option value={country.id_pays}>{country.nameFr}</option>)
               })}
-            </select>
+            </datalist>
           </div>
 
           <div className="monthly">
             <h1>Période:</h1>
-            <label htmlFor="month"></label>
-            <select name="month" id="month">
-              <option value="1" onChange={evt => setMonth(evt.target.value)}>Janvier</option>
-              <option value="2" onChange={evt => setMonth(evt.target.value)}>Février</option>
-              <option value="3" onChange={evt => setMonth(evt.target.value)}>Mars</option>
-              <option value="4" onChange={evt => setMonth(evt.target.value)}>Avril</option>
-              <option value="5" onChange={evt => setMonth(evt.target.value)}>Mai</option>
-              <option value="6" onChange={evt => setMonth(evt.target.value)}>Juin</option>
-              <option value="7" onChange={evt => setMonth(evt.target.value)}>Juillet</option>
-              <option value="8" onChange={evt => setMonth(evt.target.value)}>Aout</option>
-              <option value="9" onChange={evt => setMonth(evt.target.value)}>Septembre</option>
-              <option value="10" onChange={evt => setMonth(evt.target.value)}>Octobre</option>
-              <option value="11" onChange={evt => setMonth(evt.target.value)}>Novembre</option>
-              <option value="12" onChange={evt => setMonth(evt.target.value)}>Décembre</option>
-            </select>
+            <label htmlFor="month" ></label>
+            <input list="month" onChange={evt => setMonth(evt.target.value)} />
+            <datalist name="month" id="month">
+              {mois.map((country, i) => {
+                return (<option value={i + 1}>{country}</option>)
+              })}
+            </datalist>
             <label htmlFor="year"></label>
             <input type="number" id="year" placeholder="1970" min="1970" max="2020" onChange={evt => setYear(evt.target.value)}></input>
           </div>
 
-          <h3>Tu es parties {pays} au mois de {month} en {year}</h3>
+          <div>
+            <input type="radio" value="1" id="" checkCoice1 name="check" onChange={evt => setCheck(evt.target.value)} />
+            <label htmlFor="checkCoice1">Fait</label>
+            <input type="radio" value="0" id="" checkCoice2 name="check" onChange={evt => setCheck(evt.target.value)} />
+            <label htmlFor="checkCoice2">A faire</label>
+          </div>
+          {check === null
+            ? ""
+            : <h3>{check === "1" ? "Tu es parties" : "Tu vas partir"} {pays} au mois de {mois[parseInt(month) - 1]} en {year}</h3>
+          }
+
           <input type="submit" className="valider" />
         </form>
       </div>
