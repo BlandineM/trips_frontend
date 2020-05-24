a6857c3a44092030e279956c2c1dd8800d050daf
Blandine <blandine.mallart@gmail.com>
2020-05-24 20:37:27 +0200

    fix(info) delete warning

--- src/components/pages/profil/info/Infos.jsx
+++ src/components/pages/profil/info/Infos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import axios from 'axios';
 import "./infos.scss"
 
@@ -44,42 +44,45 @@ function Infos() {
   return (
     <div>
       {profil.map((user, i) => {
-        if (i === 0) {
-          return (
+        return (
+          (i === 0)
+            ?
+            (
 
-            <div className="profil">
+              <div className="profil">
 
-              <div className="picture">
-                <form>
-                  <label className='custom-file-label' htmlFor='customFile'>
-                    {preview ? <img src={preview} />
-                      : (<img src={(user.avatar != null
-                        ? `${user.avatar}`
-                        : 'https://res.cloudinary.com/blandine/image/upload/v1585844046/avatar/none.png')}
-                        alt='image de profil'></img>
-                      )}
-                  </label>
-                  <input
-                    style={{ display: 'none' }}
-                    type='file'
-                    className='custom-file-input'
-                    id='customFile'
-                    accept="image/x-png,image/gif,image/jpeg"
-                    onChange={onChange}
-                  >
-                  </input>
-                </form>
+                <div className="picture">
+                  <form>
+                    <label className='custom-file-label' htmlFor='customFile'>
+                      {preview ? <img src={preview} alt="profil" />
+                        : (<img src={(user.avatar != null
+                          ? `${user.avatar}`
+                          : 'https://res.cloudinary.com/blandine/image/upload/v1585844046/avatar/none.png')}
+                          alt='profil'></img>
+                        )}
+                    </label>
+                    <input
+                      style={{ display: 'none' }}
+                      type='file'
+                      className='custom-file-input'
+                      id='customFile'
+                      accept="image/x-png,image/gif,image/jpeg"
+                      onChange={onChange}
+                    >
+                    </input>
+                  </form>
+                </div>
+
+                <div>
+                  <h2 className="name">{user.user_name}</h2>
+                  <h2 className="countries">{toPassed.length} pays visité{toPassed.length > 1 ? "s" : ""}</h2>
+                </div>
+
+
               </div>
-
-              <div>
-                <h2 className="name">{user.user_name}</h2>
-                <h2 className="countries">{toPassed.length} pays visité{toPassed.length > 1 ? "s" : ""}</h2>
-              </div>
-
-
-            </div>
-          )
-        }
+            )
+            : ""
+        )
       })}
 
     </div>
