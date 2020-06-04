77ab9647ce60fb585ec8f4b94b276fa00728e7e5
Blandine <blandine.mallart@gmail.com>
2020-06-04 14:48:45 +0200

    fix(lastNextTrip) add router for add a trip and sentence when no trip

--- src/components/pages/profil/last-nextTrip/LastNextTrip.jsx
+++ src/components/pages/profil/last-nextTrip/LastNextTrip.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import "./lastNextTrip.scss"
 import TripList from './list/TripList';
+import { NavLink } from "react-router-dom";
 
 function LastNextTrip() {
   const [check, setCheck] = useState('aFaire');
@@ -42,58 +43,76 @@ function LastNextTrip() {
     }
     return (`${compteur} ${unite}`)
   }
-
   return (
     <div>
-      {toNext.map((country, i) => {
-        return (
-          i === 0
-            ? (
-              <div className="nextTrip" onClick={() => { setCheck('aFaire') }}>
-                <div className="date">
-                  <h1>{mois[country.month]}</h1>
-                  {country.year != null ? (<h1>{country.year}</h1>) : (<h1>Année à définir</h1>)}
-                </div>
-                <div className="pays">
-                  <img src={country.flag} alt="flag of country" />
-                  <div>
-                    <h1 className="country_name">{country.country_name}</h1>
+      {toNext.length > 0
+        ? toNext.map((country, i) => {
+          return (
+            i === 0
+              ? (
+                <div className="nextTrip" onClick={() => { setCheck('aFaire') }}>
+                  <div className="date">
+                    <h1>{mois[country.month]}</h1>
+                    {country.year != null ? (<h1>{country.year}</h1>) : (<h1>Année à définir</h1>)}
+                  </div>
+                  <div className="pays">
+                    <img src={country.flag} alt="flag of country" />
+                    <div>
+                      <h1 className="country_name">{country.country_name}</h1>
 
-                    <h2 className="compte">Dans {compteur(country)}</h2>
+                      <h2 className="compte">Dans {compteur(country)}</h2>
 
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-            : ""
-        )
-      })}
+              )
+              : ""
+          )
+        })
+        : (<div className="nextTrip">
+          <div className="empty">
+            <h1 className="nextTripEmpty">Tu n'as pas de prochain voyages de prévu</h1>
+            <NavLink to={`/new`} className="newTrip">
+              <h1 className="addTrip Next">+</h1>
+            </NavLink>
+          </div>
+        </div>)
+      }
       {check === 'aFaire'
         ? <TripList check={check} />
         : ""}
 
-
-      {toPassed.map((countryLast, i) => {
-        return (
-          i === 0
-            ? (
-              <div className="lastTrip" onClick={() => { setCheck('fait') }}>
-                <div className="pays">
-                  <div>
-                    <h1 className="country_name">{countryLast.country_name}</h1>
-                    <h2 className="compte">Il y a  {compteur(countryLast)}</h2>
+      {toPassed.length > 0
+        ? (toPassed.map((countryLast, i) => {
+          return (
+            i === 0
+              ? (
+                <div className="lastTrip" onClick={() => { setCheck('fait') }}>
+                  <div className="pays">
+                    <div>
+                      <h1 className="country_name">{countryLast.country_name}</h1>
+                      <h2 className="compte">Il y a  {compteur(countryLast)}</h2>
+                    </div>
+                    <img src={countryLast.flag} alt="flag of country" />
                   </div>
-                  <img src={countryLast.flag} alt="flag of country" />
+                  <div className="date">
+                    <h1>{mois[countryLast.month]}</h1>
+                    {countryLast.year != null ? (<h1>{countryLast.year}</h1>) : (<h1>Année à définir</h1>)}
+                  </div>
                 </div>
-                <div className="date">
-                  <h1>{mois[countryLast.month]}</h1>
-                  {countryLast.year != null ? (<h1>{countryLast.year}</h1>) : (<h1>Année à définir</h1>)}
-                </div>
-              </div>
-            )
-            : ""
-        )
-      })}
+              )
+              : ""
+          )
+        }))
+        : (<div className="lastTrip">
+          <div className="empty">
+            <NavLink to={`/new`} className="newTrip">
+              <h1 className="addTrip">+</h1>
+            </NavLink>
+            <h1 className="lastTripEmpty">Tu n'as pas de voyages effectué</h1>
+          </div>
+        </div>
+        )}
       {check === 'fait'
         ? <TripList check={check} />
         : ""}
